8d805b888868afb3530c2d22fec859ae12df255d
Daniela <daniela.gallardo.reyes@gmail.com>
2019-11-19 20:41:44 +0100

    commit

---
+++ corporativa/js/main.js
@@ -0,0 +1,12 @@
+
+/*fn para el menú: al hacer scroll se despliega un fondo */
+const header = document.querySelector('.main-header');
+
+window.addEventListener('scroll', ()=>{
+    const scrollPos = window.scrollY;
+        if (scrollPos > 10){
+            header.classList.add('scrolled');
+        }else{
+            header.classList.remove('scrolled');
+        }
+});
